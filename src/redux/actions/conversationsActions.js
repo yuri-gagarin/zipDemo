5267267bf31d4ce2f2783d189faa1bd3dc3b1238
Yuriy Ivanov <ymivanov@icloud.com>
2020-01-17 18:45:12 +0200

    adding redux-thunk and conversationsActions

--- src/redux/actions/conversationsActions.js
+++ src/redux/actions/conversationsActions.js
@@ -1,10 +1,103 @@
 import { conversationConstants } from "../constants/actionConstants";
 
+
+const mockRequest = () => {
+  return new Promise((resolve, reject) => {
+    const convoResponse = {
+      status: 200,
+      message: "success",
+      conversations: [
+        { _id: 21321, 
+          messages: [
+            {
+              sender: "vendor1", message: " sorry but that doesnt work"
+            }
+          ]
+        }
+      ]
+    };
+    setTimeout(() => {
+      resolve(convoResponse);
+    }, 1000);
+  });
+};
+
 const { 
+  FETCH_CONVERSATIONS,
+  UPDATE_CONVERSATIONS,
+  REQUEST_CONVERSATIONS,
   OPEN_CONVERSATION,
   CLOSE_CONVERSATION,
   DELETE_CONVERSATION,
+  CONVERSATIONS_ERROR
 } = conversationConstants;
+
+/**
+ * Notifies state of conversations request
+ * @returns {Object} A redux action
+ */
+export const requestConversations = () => {
+  return {
+    type: REQUEST_CONVERSATIONS,
+    payload: {
+      message: "Refreshing",
+      loading: true,
+      conversationsError: null
+    }
+  };
+};
+
+/**
+ * Notifies state of conversations error
+ * @param {Object} errorResponse - An API error response
+ * @returns {Object} A redux action
+ */
+export const conversationsError = (errorResponse) => {
+  return {
+    type: CONVERSATIONS_ERROR,
+    payload: {
+      message: errorResponse.message,
+      loading: false,
+      conversationsError: errorResponse
+    }
+  };
+};
+
+/**
+ * Updates conversations state after API request
+ * @param {Object} newData - New data for the state
+ * @param {string} newData.message - An API response message
+ * @param {Object[]} newData.conversations - An array of Conversation objects
+ * @returns {Object} A redux action
+ */
+export const updateConversations = ({ message, conversations }) => {
+  return {
+    type: UPDATE_CONVERSATIONS,
+    payload: {
+      message: message,
+      loading: false,
+      conversations: [...conversations],
+      conversationsError: null
+    }
+  };
+};
+
+export const fetchAndUpdateConversations = (currentConversations) => {
+
+  return function (dispatch) {
+    // notify of an API call //
+    dispatch(conversationsRequest);
+    // API CALL here in the future //
+    // mockRequest for now //
+    return mockRequest()
+      .then((response) => {
+
+      })
+      .catch((error) => {
+
+      })  
+  };
+};
 
 export const openConversation = (conversationId) => {
   return {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
 import indexReducer from "./reducers/indexReducer";
 
+const middleware = applyMiddleware([ReduxThunk]);
 // default store export //
-export default createStore(indexReducer);
+export default createStore(indexReducer, middleware);
