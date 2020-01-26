4ea918b9af542af55217b9835763c5f1d038f094
Yuriy Ivanov <ymivanov@icloud.com>
2020-01-27 00:45:09 +0200

    further camera implemntation

--- src/components/camera/CameraComponent.js
+++ src/components/camera/CameraComponent.js
@@ -3,24 +3,30 @@ import { SafeAreaView, View } from "react-native";
 import PropTypes from "prop-types";
 // additional imports and dependencies //
 import { RNCamera } from "react-native-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CameraLoadingScreen = (props) => {
   return (
-    <View style={{flex: 1, backgroundColor: "red"}}>
-
+    <View style={{flex: 1, backgroundColor: "red", justifyContent: "center", alignItems: "center"}}>
+      <Text>Loading</Text>
     </View>
-  )
+  );
 };
 
 const CameraComponent = (props) => {
   const cameraType = RNCamera.Constants.Type.back;
   const READY = RNCamera.Constants.CameraStatus.READY;
-  const flasMode = RNCamera.Constants.FlashMode.on;
+  const flashMode = RNCamera.Constants.FlashMode.on;
   const androidPermissions = {
     title: "Camera Permission",
     message: "Can we use the device camera to scan QR code?",
     buttonPositive: "Yes",
     buttonNegative: "Cancel"
+  };
+
+  const takePicture = (camera) => {
+    // take picture implementation here //
+    console.info("taking picture");
   };
 
   return (
@@ -29,11 +35,21 @@ const CameraComponent = (props) => {
       <RNCamera
         style={{flex: 9}}
         type={cameraType}
-
+        flashMode={flashMode}
+        androidCameraPermissionOptions={androidPermissions}
       >
       {
-        ({ camera, status, recordAudiPermissionStatus}) => {
-          
+        ({ camera, status, recordAudioPermissionStatus}) => {
+          if (status !== READY) {
+            return <CameraLoadingScreen />
+          }
+          return (
+            <View style={{flex: 1, flexDirection: "row", justifyContent: "center"}}>
+              <TouchableOpacity onPress={() => takePicture(camera)}>
+                <Text style={{fontSize: 14}}>Snap</Text>
+              </TouchableOpacity>
+            </View>
+          )
         }
       }
       </RNCamera>
